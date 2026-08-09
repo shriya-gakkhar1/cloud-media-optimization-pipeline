8791fb25017a82732053122c9c191d4c2878c3a9
shriya-gakkhar1 <sgakkhar2005@gmail.com>
2026-08-10 00:32:43 +0530

    chore: add PostgreSQL deployment support

--- backend/database.py
+++ backend/database.py
@@ -3,10 +3,17 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from config import settings
 
-engine = create_engine(
-    settings.DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+
+if settings.DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        settings.DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(
+        settings.DATABASE_URL
+    )
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
